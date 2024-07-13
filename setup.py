63ee6d5b0fe6b1010e2738387d19217d32f5a7db
Priyal Chugh <layirps06@gmail.com>
2024-07-13 18:42:49 +0530

    after adding -e.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from typing import List
+
+
+
+
+
+setup(
+name='sensor',
+version="0.0.1",
+author='priyal',
+author_email='priyalchugh06@gmail.com',
+packages = find_packages() ,
+install_requires =['pymongo']
+
+
+)
+
+
+
+
